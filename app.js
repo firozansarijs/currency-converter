e27fdf55f76e70f80f046c58860fc1f2bac4b6bc
firozansarijs <firoz.ansari.dev@gmail.com>
2026-07-17 21:16:57 +0530

    Rename variable dropdowns1 to dropdowns

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const BASE_URL =
   "https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies";
 
-const dropdowns1 = document.querySelectorAll(".dropdown select");
+const dropdowns = document.querySelectorAll(".dropdown select");
 const btn = document.querySelector("form button");
 const fromCurr = document.querySelector(".from select");
 const toCurr = document.querySelector(".to select");
